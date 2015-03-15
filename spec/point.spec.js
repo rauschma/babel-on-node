8a629241f0159e5554a0d91495013ededde0ec1f
rauschma <axel@rauschma.de>
2015-03-15 22:34:38 +0100

    Using arrow functions for Jasmine API.

--- spec/point.spec.js
+++ spec/point.spec.js
@@ -1,8 +1,8 @@
 jest.autoMockOff();
 import { Point } from '../point';
 
-describe('Point', function() {
-    it('sets up instance properties correctly', function() {
+describe('Point', () => {
+    it('sets up instance properties correctly', () => {
         let p = new Point(1, 5);
         console.log(JSON.stringify(p));
         expect(p.x).toBe(1);
